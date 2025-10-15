f123fb31c30b437ffc573ca499b7dab4107a9aa9
alexandros-antonorsi <alexandrosantonorsi@me.com>
2025-10-14 21:23:30 -0500

    Tabular Q learning implemented

--- scripts/train-qlearn-frozenlake.py
+++ scripts/train-qlearn-frozenlake.py
@@ -3,27 +3,29 @@ import numpy as np
 import argparse
 import pandas as pd
 from datetime import date
-from ..src.agents.qlearn_tabular import QLearnAgent
+from src.agents.qlearn_tabular import QLearnAgent
 
 #input training arguments and hyperparameters from CLI
 parser = argparse.ArgumentParser()
-parser.add_argument("--episodes", help="number of episodes to train", type=int, default=1e5)
+parser.add_argument("--episodes", help="number of episodes to train", type=int, default=int(1e5))
 parser.add_argument("--seed", help="seed for rng", type=int, default=25)
 parser.add_argument("--learn", help="learning rate", type=float, default=0.01)
-parser.add_argument("--epsilon", help="exploration rate", type=float, default=0.1)
+parser.add_argument("--e_init", help="initial exploration rate", type=float, default=1)
+parser.add_argument("--e_final", help="final exploration rate", type=float, default=0.1)
 parser.add_argument("--discount", help="discount rate", type=float, default=0.95)
 args = parser.parse_args()
-n_eps, seed, learn_rate, epsilon, discount = args.episodes, args.seed, args.learn, args.epsilon, args.discount
+n_eps, seed, learn_rate, e_init, e_final, discount = args.episodes, args.seed, args.learn, args.e_init, args.e_final, args.discount
 
 #set up env and log, make agent
-env = gym.make('FrozenLake-v1', is_slippery=False)
+env = gym.make('FrozenLake-v1', is_slippery=True)
+#env.metadata['render_fps']=240
 env.reset(seed=seed)
 env.action_space.seed(seed)
-np.random.seed(seed)
-log = pd.DataFrame(columns=['total_reward', 'episode_length', 'final_td_error'], index =[i+1 for i in range(n_eps)])
+log = pd.DataFrame(columns=['total_reward', 'episode_length'], index =[i+1 for i in range(n_eps)])
 log.index.name='episode_num'
 
-qlearn = QLearnAgent(env, learn_rate, discount, epsilon)
+e_decay = (e_init-e_final) / n_eps * 2
+qlearn = QLearnAgent(env, seed, learn_rate, discount, e_init, e_final, e_decay)
 
 for i in range(n_eps):
     #iteration variables
@@ -35,7 +37,7 @@ for i in range(n_eps):
     total_reward = 0
     episode_length = 0
 
-    while not terminated or truncated:
+    while not (terminated or truncated):
         action = qlearn.choose_action(obs)
 
         next_obs, reward, terminated, truncated, info = env.step(action)
@@ -46,16 +48,17 @@ for i in range(n_eps):
 
         total_reward += reward
         episode_length += 1
+
+    qlearn.decay()
     
-    episode_log = [total_reward, episode_length, qlearn.td_errors[-1]]
+    episode_log = [total_reward, episode_length]
     log.iloc[i] = episode_log
 
 env.close()
 
-log.to_csv(f"results/frozenlake_qlearn_{date.today()}_{seed}.csv")
+log.to_csv(f"results/frozenlake_qlearn_{date.today()}_seed{seed}.csv")
 
 print(f"Finished {n_eps} episodes on FrozenLake-v1 (seed={seed})")
-print("Average reward over last 100 eps: ",np.sum(log['total_rewards'][:-100])/100)
-print(f"Results saved to results/frozenlake_qlearn_{date.today()}_{seed}.csv")
+print("Average reward over last 100 eps: ",np.sum(log['total_reward'][-100:])/100)
+print(f"Results saved to results/frozenlake_qlearn_{date.today()}_seed{seed}.csv")
 
-    
--- src/agents/qlearn_tabular.py
+++ src/agents/qlearn_tabular.py
@@ -7,31 +7,38 @@ class QLearnAgent:
     def __init__(
             self, 
             env: gym.Env, 
+            seed: int,
             learn_rate: float,
             discount: float,
-            epsilon: float):
+            e_init: float,
+            e_final: float,
+            e_decay: float):
         self.env = env
         self.learn_rate = learn_rate
         self.discount = discount
-        self.epsilon = epsilon
+        self.epsilon = e_init
+        self.epsilon_final = e_final
+        self.decay_rate = e_decay
+        self.rng = np.random.default_rng(seed)
         
         #instead of instantiating for all state,action pairs, use defaultdict so we can only store values for observed states
         self.q_table = defaultdict(lambda: np.zeros(env.action_space.n))
 
-        #log errors for final results
-        self.td_errors = []
 
     def update(self, curr_obs, action, reward, next_obs, terminated):
         q_now = self.q_table[curr_obs][action]
         #if our action results in termination, our future rewards are 0 so we should remove that term from our approximation update
         target = reward + (1 - terminated) * self.discount * np.max(self.q_table[next_obs])
         self.q_table[curr_obs][action] = q_now - self.learn_rate * (q_now - target)
-        self.td_errors.append(q_now - target)
+
 
     #we use an epsilon-greedy policy. in Gymnasium environments the action space is the same for all states, so we don't need to store/update policy values per state
     def choose_action(self, curr_obs):
-        if np.random.random() < self.epsilon: #explore
+        if self.rng.random() < self.epsilon:
             return self.env.action_space.sample()
-        else: #greedy
-            return np.argmax(self.q_table[curr_obs])
+        else: 
+            return int(np.argmax(self.q_table[curr_obs]))
+        
+    def decay(self):
+        self.epsilon = max(self.epsilon_final, self.epsilon-self.decay_rate)
 
